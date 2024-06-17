e996ae7462bf3b14b9a2e87d23bc39b375898317
Hrushi2010 <hrushikeshmorey20@gmail.com>
2024-06-17 21:05:08 +0530

    Second

--- gym/urls.py
+++ gym/urls.py
@@ -4,10 +4,16 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('program/<int:program_id>/', views.program_detail, name='program_detail'),
+    
+
+
+
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    # Add other paths here
+
+
 ]
 
 
-from django.conf import settings
-from django.conf.urls.static import static
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- gym/views.py
+++ gym/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 from .models import Category, Program, Trainer
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login, authenticate
+
 
 def home(request):
     categories = Category.objects.all()
@@ -13,3 +17,28 @@ def home(request):
 def program_detail(request, program_id):
     program = Program.objects.get(id=program_id)
     return render(request, 'gym/program_detail.html', {'program': program})
+
+
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('home')  # Adjust 'home' to your main landing page
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('home')  # Adjust 'home' to your main landing page
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
